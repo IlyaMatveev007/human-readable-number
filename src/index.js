ec0c42a8e00a69995bd50e0236f347e396fee7ba
DESKTOP-IVVQ3M0\Ilya <ilya.matveev007@gmail.com>
2020-10-18 20:22:12 +0400

    feat: complete index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function toReadable (number) {
-  
+    var numberToWords = require('number-to-words');
+    var ans1 = numberToWords.toWords(number);
+    var ans2 = '';
+    for (var i = 0; i < ans1.length; i++){
+        if (ans1[i] !== '-'){
+            ans2 += ans1[i];
+        }
+        else{
+            ans2 += ' ';
+        }
+    }
+    return ans2;
 }
